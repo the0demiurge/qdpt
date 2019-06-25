41f98b1f68a74c8f165518194bbeee2a798b6153
Charles Xu <charl3s.xu@gmail.com>
2019-06-25 20:10:44 +0800

    changed from raw file to assets_rc.py

--- src/ui/generate_py.py
+++ src/ui/generate_py.py
@@ -2,4 +2,6 @@
 import os
 
 for file in [i for i in os.listdir('.') if i.endswith('.ui')]:
-    os.system('pyuic5 {} -o ../pyqdpt/views/ui_{}.py'.format(file, file.rstrip('.ui')))
+    os.system('pyuic5 --from-imports {} -o ../pyqdpt/views/ui_{}.py'.format(file, file.rstrip('.ui')))
+
+os.system('pyrcc5 -no-compress {} -o ../pyqdpt/views/assets_rc.py'.format('../../assets/assets.qrc'))
